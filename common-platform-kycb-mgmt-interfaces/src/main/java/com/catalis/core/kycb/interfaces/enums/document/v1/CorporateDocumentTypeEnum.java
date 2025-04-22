7d90ea26f2c1671f54ca3b4b45a66cb7c60a611c
Andrés Contreras Guillén <andres.contreras@dworkers.ai>
2025-04-22 15:16:37 +0200

    Add missing enum

---
+++ common-platform-kycb-mgmt-interfaces/src/main/java/com/catalis/core/kycb/interfaces/enums/document/v1/CorporateDocumentTypeEnum.java
@@ -0,0 +1,12 @@
+package com.catalis.core.kycb.interfaces.enums.document.v1;
+
+/**
+ * Enum representing the types of corporate documents.
+ */
+public enum CorporateDocumentTypeEnum {
+    DEED_OF_INCORPORATION,
+    BYLAWS,
+    POWER_OF_ATTORNEY,
+    BOARD_RESOLUTION,
+    TAX_ID
+}
